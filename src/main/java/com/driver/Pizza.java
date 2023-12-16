a11e7d967b8f7e9c75797408b9d95907ac00139c
Piyush Verma <piyushvrm265@gmail.com>
2023-12-16 14:28:56 +0530

    Submitting pizza bill

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,6 +5,9 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean isCheeseAdded;
+    private boolean isToppingsAdded;
+    private boolean isTakeaway;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
@@ -17,18 +20,40 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isCheeseAdded){
+            isCheeseAdded = true;
+            System.out.println("Extra Cheese Added: 80");
+            price+=80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isToppingsAdded){
+            isToppingsAdded = true;
+            if(isVeg){
+                System.out.println("Extra Toppings Added: 70");
+                price+=70;
+            }
+            else{
+                System.out.println("Extra Toppings Added: 120");
+                price+=120;
+            }
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeaway){
+            System.out.println("Paper bag Price = 20");
+            price+=20;
+            isTakeaway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        String bill = "Total Price: "+price;
+        return bill;
     }
 }
